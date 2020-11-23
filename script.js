762e5bef4ef4bac308c04608b33f4514d26afd1c
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2020-11-23 18:35:40 +0100

    js countdown and game over overlay done

--- script.js
+++ script.js
@@ -5,13 +5,27 @@ const scoreEl = document.getElementById('score');
 const randomWordEl = document.getElementById('random-word');
 const text = document.getElementById('text');
 const gameOverOverlay = document.getElementById('game-over-overlay');
-const reloadBtn = document.getElementById('reload-btn');
+// const reloadBtn = document.getElementById('reload-btn');
 
 text.focus();
 
 let time = 10;
 let score = 0;
 let randomWord;
+
+
+// Don't know for now
+// function getWord() {
+//   fetch('https://github.com/dariusk/corpora/blob/master/data/words/common.json')
+
+//     .then(data => {
+
+//       console.log(data);
+//     });
+//   ;
+// }
+
+// getWord();
 
 let wordsArr = ['words', 'simple', 'selection', 'whereas', 'sometimes', 'pictures', 'milestone', 'bootle', 'notebook', 'drawer'];
 
@@ -22,17 +36,54 @@ function getRandomWord() {
 
 getRandomWord()
 
-console.log(randomWord);
+// console.log(randomWord);
+
+function updateScore() {
+  score++;
+  scoreEl.innerText = score;
+}
+
+function updateTime() {
+  time += 3;
+  timeEl.innerText = time + 's';
+}
 
 function checkWord(e) {
   if (e.target.value === randomWord) {
     getRandomWord();
+    updateScore();
+    updateTime();
     text.value = '';
   }
+}
 
+const timeInterval = setInterval(setTime, 1000);
+
+function setTime() {
+  time--;
+  timeEl.innerText = time + 's';
+
+  if (time === 0) {
+    gameOver()
+    clearInterval(time)
+  }
+}
+
+setTime();
+
+function gameOver() {
+  gameOverOverlay.style.display = 'flex';
+  gameOverOverlay.innerHTML = `
+  <h1>Game Over</h1>
+    <p>Your score is: <span>${score}</span></p>
+    <button id="reload-btn" onclick="reloadGame()">Reload</button>
+  `
+}
+
+function reloadGame() {
+  location.reload();
 }
 
 // Event Listeners
 text.addEventListener('input', checkWord);
-
 
